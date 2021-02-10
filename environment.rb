92dfb847515f572a47d9a79e93f7ac8fc65e28b4
Tara Harwood <tara.m.harwood@gmail.com>
2021-02-10 14:26:27 -0800

    refactored web, starting db work

--- environment.rb
+++ environment.rb
@@ -1,11 +1,31 @@
 require 'pry'
 require_relative './maslow.rb'
-
-#why can't I just point to the path here?
-# require_relative './lib'
 require_relative './lib/member.rb'
 require_relative './lib/post.rb'
 require_relative './lib/comment.rb'
+
+## database
+
+require 'sqlite3'
+
+begin
+
+db = SQLite3::Database.open "maslow.db"
+db.execute "INSERT INTO Members VALUES ('Echo')"
+db.execute "INSERT INTO Members VALUES ('Foxtrot')"
+db.execute "INSERT INTO Members VALUES ('Golf')"
+db.execute "INSERT INTO Members VALUES ('Hotel')"
+
+rescue SQLite3::Exception => e 
+    
+    puts "Exception occurred"
+    puts e
+    
+ensure
+    db.close if db
+end
+
+### execute
 
 runner
 
